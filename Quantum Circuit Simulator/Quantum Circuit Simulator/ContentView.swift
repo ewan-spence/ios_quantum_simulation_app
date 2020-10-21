d4aa78e5916df02fdc8bfeca8b574563b293792e
ewan-spence <ewan_spence@btinternet.com>
2020-10-21 17:22:51 +0100

    Adds empty iOS project

---
+++ Quantum Circuit Simulator/Quantum Circuit Simulator/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Quantum Circuit Simulator
+//
+//  Created by Ewan Spence on 21/10/2020.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
